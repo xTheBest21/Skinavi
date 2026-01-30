28b9e0fd767343583439f888cda684a0401ceebc
xTheBest21 <jannikohnhaeuser@yahoo.de>
2026-01-30 13:30:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -88,8 +88,8 @@ def build_soelden_graph():
 # --- UI ---
 # 5. UI INITIALISIEREN
 st.title("⛷️ Ski Navi Sölden")
-start = st.sidebar.selectbox("Start", sorted(nodes.keys()))
-ziel = st.sidebar.selectbox("Ziel", sorted(nodes.keys()))
+start = st.sidebar.selectbox("Start", sorted(nodes.keys())
+ziel = st.sidebar.selectbox("Ziel", sorted(nodes.keys())
 show_coords = st.sidebar.checkbox("Koordinaten-Helfer anzeigen")
 
 # Fehlerprüfung
