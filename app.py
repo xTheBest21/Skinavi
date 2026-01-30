5ebd60de1345d054aa48ddecaf8db2d9ffb41c35
xTheBest21 <jannikohnhaeuser@yahoo.de>
2026-01-30 14:40:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ show_coords = st.sidebar.checkbox("Koordinaten-Helfer (für neue Punkte)")
 # --- KARTE INITIALISIEREN ---
 map_bounds = [[0, 0], [1000, 1400]]
 m = folium.Map(crs='Simple', location=[500, 700], zoom_start=-0.5,
-tiles=None
+tiles=None,
 background_color="white")
 # Pistenplan Overlay
 folium.raster_layers.ImageOverlay(
